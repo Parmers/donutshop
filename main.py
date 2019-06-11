0094d9da6c4035afa5532bf8d6208b7f54898e2d
Elisa <wheaton-cs@outlook.com>
2019-06-11 19:44:49 -0400

    generate an empty window
    you can run --> $ python3 main.py 
    in the terminal if you can't get the virtual env to work

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-import pygame
+import pygame, sys
 
 #Summer 2019 Python Game Project - Ashad's Donut Shop
+
+from pygame.locals import *
+
+pygame.init()
+
+DISPLAYSURF = pygame.display.set_mode((1000,700))
+
+pygame.display.set_caption("Ashad's Donut Shop")
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+    pygame.display.update()
